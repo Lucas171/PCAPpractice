65f900aa4bce207ca924971e50093bedbb294109
Lucas Miller <Lucasmiller401@gmail.com>
2024-01-05 08:52:26 -0500

    Delete PCAPpractice/week1 directory

--- PCAPpractice/week1/nestedMods/multiplication.py
+++
@@ -1,9 +0,0 @@
-from addition import addition
-
-def multiply(num1, num2):
-  return num1 * num2
-
-if __name__ == '__main__':
-  added_num = addition(5, 5) # 10
-  answer = multiply(added_num, 10) # 100
-  print(answer)
